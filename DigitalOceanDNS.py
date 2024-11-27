94b90cfe22886d47fb22060940e0ce240f5a71e7
Max Tran <76268730+SpamMusubi153@users.noreply.github.com>
2024-11-26 17:16:19 -0800

    Update with Local Use Support

--- DigitalOceanDNS.py
+++ DigitalOceanDNS.py
@@ -8,28 +8,34 @@ import json
 # This script uses the Digital Ocean Domain Record APIs to update all of the "A" DNS records
 # for a specific set of domains. It is written solely using Python and the Python Standard Library.
 
-# When run as a Digital Ocean function, it is intended to update DNS records for a webserver 
-# on a dynamically assigned IP address. This makes it easier and more feasible to run a webserver in
-# unstable network situations, such as residential IPs, while still using a domain name.
+# This script can be used in one of two modes:
+# 1: As a free dynamic IP service you can run on your own server to ensure your DigitalOcean domains stay
+#    up to date with your public IP address. This makes it easier and more feasible to run a webserver in
+#    unstable network situations, such as residential IPs, while still using a domain name.
+# 2: As an IP mirror service. Choose this option if you already use a dynamic IP service, such as No-IP
+#    and want to keep your DigitalOcean domains pointed to your most recent IP. If you choose this option,
+#    you don't necessarily have to run the script on your own server, and can even run it for free using
+#    DigitalOcean functions!
 
-# This script assumes the user already has access to a free dynamic IP service, such as No-IP that can
-# be queried to provide the location of the server. It then enables a user to extend the service
-# to other domains served by the same server.
-
-# It is also possible to run this script on your own server, or in other scenarios by appropriately
+# It is also possible to easily update this script for use in other scenarios by appropriately
 # updating the get_server_ip function.
 
 # To get started, fill out the "Required Values" section of the script below. Then, copy the entire file
 # into the main function of a Digital Ocean "function". You can then schedule the script to regularly 
 # update the DNS records of your websites.
 
-# Good luck on your webhosting projects!
+# Good luck with your webhosting projects!
 
 # ――――――――――――――――――――― #
 # Environment Variables #
 # ――――――――――――――――――――― #
 
-# 1. Required Values
+# 
+# 1A. Required Values for All Modes
+# 
+
+# Select your mode by referring to the mode descriptions above.
+SELECTED_MODE = 1
 
 # A list of the domains 
 DOMAINS_TO_UPDATE = [
@@ -42,15 +48,21 @@ SUBDOMAINS_TO_EXCLUDE = [
     "www",    
 ]
 
-# The address provided by the dynamic DNS service
-DYNAMIC_DNS_HOSTNAME = "YourDynamicServerIPHostname"
-
 # Your Digital Ocean API Key
 DIGITAL_OCEAN_API_KEY = "YourDigitalOceanAPIKey"
 
+# 
+# 1B. Required Values for Mode 2
+# 
+
+# The address provided by the dynamic DNS service
+DYNAMIC_DNS_HOSTNAME = "YourDynamicServerIPHostname"
+
 # ―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――― #
 
+# 
 # 2. Optional Values and Code
+# 
 
 # The base path of the Digital Ocean API Endpoint
 BASE_DIGITAL_OCEAN_BASE_API_URL = "https://api.digitalocean.com/v2/domains/"
@@ -59,12 +71,46 @@ BASE_DIGITAL_OCEAN_BASE_API_URL = "https://api.digitalocean.com/v2/domains/"
 #   This function obtains your server's IP address.
 def get_server_ip(hostname):
 
-    # This internal function is used allow wrapping of this code by the "attempt" function
+    # For now, internal functions are used allow wrapping of code by the "attempt" function
     # to attempt this operation multiple times if it fails.
-    def get_ip(hostname):
+
+    # Retrieve an IP from an existing host name.
+    def get_ip_from_other(hostname):
         return socket.gethostbyname(hostname)
+
+    # OR
+
+    # Retrieve the current machine's public IP Address.
+    def get_current_machine_public_ip_address():
+        QUERY_URLS = [
+            "https://api.ipify.org",
+        ]
+
+        query_ip_results = []
+
+        for i, current_url in enumerate(QUERY_URLS):
+            current_query_result = requests.get(current_url).text.strip()
+            query_ip_results.append(current_query_result)
+
+            # Check to make sure that the most recent retured IP matches all IP previously returned.
+            for j in range(0, i):
+                if current_query_result != query_ip_results[j]:
+                    raise Exception(f"The public ip query result from {current_url} ({current_query_result}) does not match the query result from {QUERY_URLS[j]} ({query_ip_results[j]}).")
+
+        return query_ip_results[0]
+
+    # Potential Future Code to Retrieve a Machine's Local IP Address
+    # def retrieve_local_ip_address():
+    #     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #     s.settimeout(0)
+    #     s.connect(("1.1.1.1", 80))
+    #     return s.getsockname()[0]
+
     
-    return attempt(get_ip, arguments=(hostname,))
+    if SELECTED_MODE == 2:  
+        return attempt(get_ip, arguments=(hostname,))
+    else:
+        return attempt(get_current_machine_public_ip_address)
 
 
 # ―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――― #
--- README.md
+++ README.md
@@ -3,17 +3,21 @@ Digital Ocean DNS for Dynamic IPs
 
 ### Welcome!
 
+# Welcome!
+
 This script uses the Digital Ocean Domain Record APIs to update all of the "A" DNS records for a specific set of domains. It is written solely using Python and the Python Standard Library.
 
-When run as a Digital Ocean function, it is intended to update DNS records for a webserver on a dynamically assigned IP address. This makes it easier and more feasible to run a webserver in unstable network situations, such as **residential IPs**, while still using a domain name.
+This script can be used in one of two modes:
 
-This script assumes the user already has access to a free dynamic IP service, such as No-IP that can be queried to provide the location of the server. It then enables a user to extend the service to other domains served by the same server.
+1. As a **free dynamic IP service** you can run on your own server to ensure your DigitalOcean domains stay up to date with your public IP address. This makes it easier and more feasible to run a webserver in unstable network situations, such as **residential IPs**, while still using a domain name.
 
-It is also possible to run this script on your own server, or in other scenarios by appropriately updating the get_server_ip function.
+ 2. As an **IP mirror service**. Choose this option **if you already use a dynamic IP service**, such as No-IP and want to keep your DigitalOcean domains pointed to your most recent IP. If you choose this option, you don't necessarily have to run the script on your own server, and can even run it for free using DigitalOcean functions!
+
+It is also possible to easily update this script for use in other scenarios by appropriately updating the get_server_ip function.
 
 To get started, fill out the "Required Values" section of the script below. Then, copy the entire file into the main function of a Digital Ocean "function". You can then schedule the script to regularly update the DNS records of your websites.
 
-**Good luck on your webhosting projects!**
+**Good luck with your webhosting projects!**
 
 <br>
 
